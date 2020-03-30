a3b25e7087f4d4d4ff5023dc6c84bd4eab405e61
Gogh <im@tiankele.cn>
2020-03-30 15:50:35 +0800

    #shell# auto commit

--- shelljs.1.js
+++ shelljs.1.js
@@ -1,7 +1,10 @@
 const shell = require('shelljs')
+console.log('git command ready start!')
+
 if(shell.exec('git add -A').code !== 0) {
   shell.echo('Error: Git command err')
   shell.exit(1)
 }
 shell.exec('git commit -m "#shell# auto commit"')
 shell.exec('git push')
+console.log('git command done!')
